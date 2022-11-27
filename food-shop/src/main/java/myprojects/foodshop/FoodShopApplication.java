18341bb6559a197f14cb2e49183b624e513aeced
KlaraKutlakova <klarakutlakova@gmail.com>
2022-11-27 19:53:43 +0100

    create project

---
+++ food-shop/src/main/java/myprojects/foodshop/FoodShopApplication.java
@@ -0,0 +1,13 @@
+package myprojects.foodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodShopApplication.class, args);
+	}
+
+}
